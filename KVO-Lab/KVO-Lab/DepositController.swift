630a85b8b237b59376171b59fa383f41c45f2218
Eric Davenport <EricDavenport@pursuit.org>
2020-04-10 12:41:35 -0400

    KVO Lab - deposit controller loading users

--- KVO-Lab/KVO-Lab/DepositController.swift
+++ KVO-Lab/KVO-Lab/DepositController.swift
@@ -9,13 +9,51 @@
 import UIKit
 
 class DepositController: UIViewController {
+  
+  @IBOutlet var tableView: UITableView!
+  private var depositObservation:  NSKeyValueObservation?
+  
+  private var users = [User]() {
+    didSet {
+//      tableView.reloadData()
+    }
+  }
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      tableView.dataSource = self
+      loadTableView()
     }
     
+  private func loadTableView() {
+    depositObservation = UserObject.shared.observe(\.users, options: [.new], changeHandler: { [weak self] (user, change) in
+      print("inside of observer")
+      guard let newUsers = change.newValue else { return }
+      self?.users = newUsers
+      self?.tableView.reloadData()
+    })
+    
+  }
+  
 
 
 
+}
+
+
+extension DepositController : UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return users.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "depositCell", for: indexPath)
+    let user = users[indexPath.row]
+    
+    cell.textLabel?.text = user.name
+    cell.detailTextLabel?.text = "Account balance: $\(user.walletAmount).00"
+    return cell
+  }
+  
 }
